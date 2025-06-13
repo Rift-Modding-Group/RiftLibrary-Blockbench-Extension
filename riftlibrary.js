ef6322be0ce9272cd90f49626b23cb329a5fcf00
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2025-06-13 14:14:07 +0800

    First commit

---
+++ riftlibrary.js
@@ -0,0 +1,10 @@
+Plugin.register('riftlibrary', {
+    title: 'RiftLibrary Blockbench Extension',
+    author: 'ANightDazingZoroark',
+    //icon: 'icon',
+    description: 'Your Description',
+    version: '1.0.0',
+    variant: 'both',
+    onload() {
+    }
+});
